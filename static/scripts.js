774279eda5879a5d147459d102d0f4f1174ee315
Ubuntu <kavya@kavya.usnjlhrlkgjunf2tmjhrmcmlxh.bx.internal.cloudapp.net>
2020-08-07 15:37:00 +0000

    changed file selection js

--- static/scripts.js
+++ static/scripts.js
@@ -1,14 +1,35 @@
-$(document).ready(function () {
-    $('.nav ul li:first').addClass('active');
+// $(document).ready(function () {
+//     $('.nav ul li:first').addClass('active');
+//
+//     $('.tab-content:not(:first)').hide();
+//
+//     $('.nav ul li a').click(function (event) {
+//         event.preventDefault();
+//         var content = $(this).attr('href');
+//         $(this).parent().addClass('active');
+//         $(this).parent().siblings().removeClass('active');
+//         $(content).show();
+//         $(content).siblings('.tab-content').hide();
+//     });
+// });
 
-    $('.tab-content:not(:first)').hide();
+$('#wavFileUpload').on('change',function(){
+                //get the file name
+                var fileName = $(this).val();
+                //replace the "Choose a file" label
+                $(this).next('.custom-file-label').html(fileName);
+            })
 
-    $('.nav ul li a').click(function (event) {
-        event.preventDefault();
-        var content = $(this).attr('href');
-        $(this).parent().addClass('active');
-        $(this).parent().siblings().removeClass('active');
-        $(content).show();
-        $(content).siblings('.tab-content').hide();
-    });
-});
+
+$('#yamlFileUpload').on('change',function(){
+                //get the file name
+                var fileName = $(this).val();
+                //replace the "Choose a file" label
+                $(this).next('.custom-file-label').html(fileName);
+            })
+$('#dictFileUpload').on('change',function(){
+                //get the file name
+                var fileName = $(this).val();
+                //replace the "Choose a file" label
+                $(this).next('.custom-file-label').html(fileName);
+            })
